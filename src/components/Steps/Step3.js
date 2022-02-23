44ca9ca00bcfbdd3a5927732f6260ebdcaa4fa56
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2022-02-23 14:02:24 +0100

    first build version

--- src/components/Steps/Step3.js
+++ src/components/Steps/Step3.js
@@ -2,7 +2,7 @@ const Step3 = () => {
   return (
     <div>
       <h2>Step 3</h2>
-      <div>it is another step</div>
+      <div>it is another step, some information will be available soon</div>
     </div>
   );
 };
